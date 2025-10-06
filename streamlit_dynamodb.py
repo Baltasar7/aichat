dda9fe4bf9080cf642696dd80a78830fc9e0e8a7
test user <test@example.com>
2025-10-07 03:16:01 +0900

    Add debug print to streamlit_dynamodb code.

--- streamlit_dynamodb.py
+++ streamlit_dynamodb.py
@@ -8,7 +8,7 @@ from langchain_community.chat_message_histories import DynamoDBChatMessageHistor
 import streamlit as st
 
 load_dotenv()
-#set_debug(True)
+set_debug(True)
 
 st.title("Bedrock Chat with Claude 4.5 Inference Profile")
 
@@ -21,6 +21,7 @@ if "history" not in st.session_state:
     session_id=st.session_state.session_id,
   )
 
+print("debug: if chain block start")
 if "chain" not in st.session_state:
   prompt = ChatPromptTemplate.from_messages(
     [
@@ -41,17 +42,23 @@ if "chain" not in st.session_state:
   )
   chain = prompt | chat
   st.session_state.chain = chain
+print("debug: if chain block end")
 
 if st.button("履歴クリア"):
   st.session_state.history.clear()
 
+print("debug: for message block start")
 for message in st.session_state.history.messages:
   with st.chat_message(message.type):
     st.markdown(message.content)
+    print("debug: message.type chat st.markdown(message.content) done")
+print("debug: for message block end")
 
+print("debug: if prompt block start")
 if prompt := st.chat_input("質問を入力してください。"):
   with st.chat_message("user"):
     st.markdown(prompt)
+    print("debug: user chat st.markdown(prompt) done")
 
   with st.chat_message("assistant"):
     response = st.write_stream(
@@ -63,6 +70,9 @@ if prompt := st.chat_input("質問を入力してください。"):
         config={"configurable": {"session_id": st.session_state.session_id}},
       )
     )
+    print("debug: assistant chat response set done")
 
   st.session_state.history.add_user_message(prompt)
   st.session_state.history.add_ai_message(response)
+  print("debug: stored in DynamoDB")
+print("debug: if prompt block end")
